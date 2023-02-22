dda55f8cf799ac5f0273348d26c62e1b81989d99
waiyan <waiyanavionics@gmail.com>
2023-02-22 13:56:42 +0630

    prepare for npm publish

---
+++ src/components/react_gauge.js
@@ -0,0 +1,63 @@
+import ReactEcharts from "echarts-for-react"; 
+import { useState } from 'react';
+
+function ReactGuage({ firstColor, secondColor, thirdColor, value, style }) {
+  const [option, setOption ] = useState({
+    series: [
+      {
+        type: 'gauge',
+        axisLine: {
+          lineStyle: {
+            width: 30,
+            color: [
+              [0.3, firstColor],
+              [0.7, secondColor],
+              [1, thirdColor]
+            ]
+          }
+        },
+        pointer: {
+          itemStyle: {
+            color: 'inherit'
+          }
+        },
+        axisTick: {
+          distance: -30,
+          length: 8,
+          lineStyle: {
+            color: '#fff',
+            width: 2
+          }
+        },
+        splitLine: {
+          distance: -30,
+          length: 30,
+          lineStyle: {
+            color: '#fff',
+            width: 4
+          }
+        },
+        axisLabel: {
+          color: 'inherit',
+          distance: 40,
+          fontSize: 20
+        },
+        detail: {
+          valueAnimation: true,
+          formatter: '{value} %',
+          color: 'inherit'
+        },
+        data: [
+          {
+            value: value
+          }
+        ]
+      }
+    ]
+  })
+
+  return <ReactEcharts option={option} style={style} />;
+}
+
+
+export default ReactGuage;
